819c9a7bf452a4cfb05f58c7392219b144fe876f
ca r214.01 <ca.r214.01@ca.local>
2023-03-02 10:19:53 +0400

    DATA TYPES

---
+++ 2. Data Types/app.js
@@ -0,0 +1,101 @@
+// Define variable
+
+// let, var, const
+
+// let number = 5;
+// let number2 = 5.6
+// let newWord = "hello";
+// let isStudent = true;
+// undefined
+// null
+// Symbol
+
+// let newNumber = null;
+// console.log("newNumber: ", newNumber)
+
+// console.log("number: ", typeof number)
+// console.log("new Word: ", typeof newWord)
+// console.log("isStudent: ", typeof isStudent)
+
+// console.log(21)
+
+// const a = 21;
+// a = 22;
+// console.log(a)
+
+// camelCase
+// snake_case
+// PascalCase
+// kabab-case
+
+// let firstNumber = 5;
+// let first_number = 10;
+// let FirstNumber = 6;
+// let first-number = 14;
+
+// let userName = "hajar";
+// let surname ;
+
+// let firstNumber = 5;
+// let secondNumber = 6;
+// let sumOfTwoNumbers = firstNumber + secondNumber;
+// console.log("sumOfTwoNumbers: ", sumOfTwoNumbers)
+
+// console.log("multiplicationOfTwoNumbers: ", firstNumber * secondNumber)
+// console.log("power of 5: ", firstNumber ** 3)
+
+// console.log("reminder: ", 32 % 6)
+
+// let a = 25;
+// console.log("a: ", a--)
+// console.log("a: ", a)
+
+// let a = console.log(5 + 6);
+// console.log(a)
+
+// console.log(5 + 6 + 'hello')
+// console.log('hello' + 5 + 6)
+
+// console.log(5 - "hello");
+// console.log(typeof NaN);
+
+// let firstNumber = 5;
+// let secondNumber = 6;
+
+// firstNumber = firstNumber + secondNumber;
+
+// firstNumber *= secondNumber
+// console.log(firstNumber)
+
+// Comparison Operators --> boolean deyer return edir
+
+// == yalniz deyerleri yoxlayir
+// === -> hem deyeri hem de type yoxlayir
+// console.log(5 == 5)
+
+// true = 1
+// false = 0
+// console.log(1 == true)
+
+
+// console.log(1 === true)
+// console.log(5 === 7)
+
+// console.log(6 != 78)
+// console.log(6 != '6')
+
+// int 6 string 6ya beraberdir?
+// console.log(6 !== "6");
+
+// console.log(5 <= "5"); 
+
+// console.log(3 > 2 >= 1)
+
+// ve --> &&
+// false ve false --> false
+// true ve false --> false
+// true ve true --> true
+
+// ve ya --> ||
+// true ve false --> true
+// console.log(5 > 7 || 9 > 1);
