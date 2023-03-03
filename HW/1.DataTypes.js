57d1797b4af2a87c0e79309a432d44b7ea6c9992
hajarabdullayeva <hajar.abdullayeva@gmail.com>
2023-03-03 08:34:04 +0400

    hw

---
+++ HW/1.DataTypes.js
@@ -0,0 +1,23 @@
+// const number = 67
+
+// const reminder = number % 10;
+// const num = (number - reminder) / 10
+
+// console.log(num, " ", reminder)
+
+//! Task2
+// const number = 198;
+// const firstDigit = parseInt(number / 100);
+// const secondDigit = number % 10;
+
+// console.log(firstDigit + "" + secondDigit);
+
+//? Code Formatter --> Shift + Alt + F
+
+//! Task 3
+// const students = 10;
+// const apples = 105;
+
+// const num = apples / students;
+
+// console.log(parseInt(num));
